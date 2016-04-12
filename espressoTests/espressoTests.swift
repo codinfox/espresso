afd990c682bbd5b53c02a7ae2380099b601568a6
Zhihao <lzh727@126.com>
2016-04-11 21:00:28 -0400

    Initial Commit

---
+++ espressoTests/espressoTests.swift
@@ -0,0 +1,36 @@
+//
+//  espressoTests.swift
+//  espressoTests
+//
+//  Created by Zhihao Li on 4/11/16.
+//  Copyright © 2016 CMU. All rights reserved.
+//
+
+import XCTest
+@testable import espresso
+
+class espressoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
