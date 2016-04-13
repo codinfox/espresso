cb77c8205d0ed56942893d0712fa2a93a469b545
Zhihao <lzh727@126.com>
2016-04-13 01:10:17 -0400

    Create the project file organisation

---
+++ espresso/solvers/Solver.swift
@@ -0,0 +1,9 @@
+//
+//  Solver.swift
+//  espresso
+//
+//  Created by Zhihao Li on 4/13/16.
+//  Copyright © 2016 CMU. All rights reserved.
+//
+
+import Foundation
---
+++ espresso/utilities/Tensor.swift
@@ -0,0 +1,32 @@
+//
+//  Tensor.swift
+//  espresso
+//
+//  Created by Zhihao Li on 4/12/16.
+//  Copyright © 2016 CMU. All rights reserved.
+//
+
+import Foundation
+
+/** 
+ Basic storage class
+ Tensor is a multidimensional matrix. This serves as the fundamental storage class. Tensor can take arbitrary type of data and when using, should be initialized with the dimension.
+ */
+public class Tensor<StorageDataType> {
+  var storage : [StorageDataType] = []
+
+  public private(set) var dimensions : [Int] = []
+  public private(set) var numel : Int = 0
+
+  /**
+   Initialize the Tensor with dimensionalities
+   */
+  init(dimensions: [Int]) {
+    self.dimensions = dimensions
+    self.numel = 1
+    for d in dimensions {
+      self.numel *= d
+    }
+    self.storage.reserveCapacity(numel)
+  }
+}
