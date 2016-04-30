f5ff820d05a6f1689ad38c8f1a0eb97bb66502b0
Zhihao <lzh727@126.com>
2016-04-30 17:25:54 -0400

    Import network weights

---
+++ espressoTests/NetworkTests.swift
@@ -0,0 +1,27 @@
+//
+//  NetworkTests.swift
+//  espresso
+//
+//  Created by Zhihao Li on 4/30/16.
+//  Copyright © 2016 CMU. All rights reserved.
+//
+
+import XCTest
+@testable import espresso
+
+class NetworkTests: XCTestCase {
+
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+
+  func testImport() {
+    
+  }
+}
